3ae261af0a9f2d7f49f7e4b739d0cccb1af83519
unknown <0015118@yankon.com>
2021-07-15 12:47:18 +0800

    清除keil编译信息

--- examples/developing/ble_mesh_PB_Remote/keil/Objects/common.d
+++
@@ -1,7 +0,0 @@
-.\objects\common.o: ..\..\..\..\Source\Common\Common.c
-.\objects\common.o: ..\..\..\..\Source\Common\Common.h
-.\objects\common.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\common.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\common.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\objects\common.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\objects\common.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
